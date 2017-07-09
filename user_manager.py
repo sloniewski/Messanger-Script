d923e68527770ea7ba241d0ef5d1ed222ddf8ad9
sloniewski <sloniewski.maciej@gmail.com>
2017-07-09 15:17:06 +0200

    user manager argparse

--- user_manager.py
+++ user_manager.py
@@ -1,6 +1,30 @@
-from lib.User_model import User
+from lib.models import User
 import argparse
 
 
 def set_options():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username",
+                        action="store", dest="username",
+                        help="User name")
+    parser.add_argument("-p", "--password",
+                        action="store", dest="password",
+                        help="User password")
+    parser.add_argument("-n", "--new-pass",
+                        action="store", dest="new-pass",
+                        help="Updated password")
+    parser.add_argument("-l", "--list",
+                        action="store_true", dest="list", default=False,
+                        help="List request")
+    parser.add_argument("-d", "--delete",
+                        action="store", dest="delete", default=False,
+                        help="Cinema name")
+    parser.add_argument("-e", "--edit",
+                        action="store", dest="email",
+                        help="User email for edit")
+    options = parser.parse_args()
+    return options
+
+
+if __name__ == '__main__':
+    pass
